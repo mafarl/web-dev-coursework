d58fddeb61aaa57e1cf994b3e72bbf8ac0677295
mafarl <108734570+mafarl@users.noreply.github.com>
2023-03-23 02:09:59 +0000

    Create registration_processing.php

---
+++ registration_processing.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+$username = $_POST['username'];
+$skin = $_POST['selectedOptionSkin'];
+$error_check = false;
+
+if(empty($username)) {
+    $_SESSION["error_set"] = "No name";
+    $error_check = true;
+    header("Location: ./registration.php");
+}
+if (!preg_match("/^[a-zA-Z ]*$/",$username)) {
+    $_SESSION["error_char"] = "Invalid name";
+    $error_check = true;
+    header("Location: ./registration.php");
+}
+if ($error_check) {
+    $_SESSION['username'] = $username;
+
+    $_SESSION['skinAvatar'] = $skin;
+    $_SESSION['eyesAvatar'] = $_POST['eyes-select'];
+    $_SESSION["mouthAvatar"] = $_POST['mouth-select'];
+
+    header("Location: ./index.php");
+}
