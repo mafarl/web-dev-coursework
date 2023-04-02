bc776ee8c3d75e305c5fb6ff3ed495e24e8af75e
mafarl <108734570+mafarl@users.noreply.github.com>
2023-04-02 01:46:17 +0100

    Update pairs-cards.js

--- pairs-cards.js
+++ pairs-cards.js
@@ -2,22 +2,23 @@ const skinImages = ['emoji-assets/skin/red.png', 'emoji-assets/skin/yellow.png',
 const eyesImages = ['emoji-assets/eyes/closed.png', 'emoji-assets/eyes/laughing.png', 'emoji-assets/eyes/long.png', 'emoji-assets/eyes/normal.png', 'emoji-assets/eyes/rolling.png', 'emoji-assets/eyes/winking.png'];
 const mouthImages = ['emoji-assets/mouth/open.png', 'emoji-assets/mouth/sad.png', 'emoji-assets/mouth/smiling.png', 'emoji-assets/mouth/straight.png', 'emoji-assets/mouth/surprise.png', 'emoji-assets/mouth/teeth.png'];
 
-let level = document.getElementById("level-storage").value;
 let numberOfCards = 0;
 
-let hasFlippedCard = false;
+let numberFlippedCard = 0;
 let lockBoard = false;
-let firstCard, secondCard;
+let arrayOfCards=[];
 let allPairs = [];
 let cardID = 0;
+
+let numMatches = 0;
 
 // Generate combination of images (skin,eyes,mouth) for the card
 function createCardCombination(skinImages, eyesImages, mouthImages){
     // Math.floor(x) - the largest integer number less or equal to x
     // Math.random() - returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1
-    const skinIndex = Math.floor(Math.random() * skinImages.length);
-    const eyesIndex = Math.floor(Math.random() * eyesImages.length);
-    const mouthIndex = Math.floor(Math.random() * mouthImages.length);
+    let skinIndex = Math.floor(Math.random() * skinImages.length);
+    let eyesIndex = Math.floor(Math.random() * eyesImages.length);
+    let mouthIndex = Math.floor(Math.random() * mouthImages.length);
     let array = [skinImages[skinIndex], eyesImages[eyesIndex], mouthImages[mouthIndex], cardID];
     if (checkIfCardExists(array,allPairs) == false){
         cardID++;
@@ -59,70 +60,102 @@ function cardsShuffle(cardArray){
 
 function flipCard() {
     if (lockBoard) return;
-    if (this === firstCard) return;
+    // Instead of checking if it's the 1st card, check  if it's NOT the last one
+    if (this === arrayOfCards[0]) return;
 
     this.classList.add('flip');
   
     //this.classList.add('flip');
     let children = this.childNodes;
     children.forEach(child => child.removeAttribute("hidden"));
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
+    if (numberFlippedCard!=(numberOfCards-1)) {
+      arrayOfCards[numberFlippedCard] = this;
+      numberFlippedCard++;
       return;
     }
   
-    secondCard = this;
+    //secondCard = this;
+    arrayOfCards[numberFlippedCard] = this;
+    numberFlippedCard++;
     checkForMatch();
 }
 
 function checkForMatch() {
-    let isMatch = firstCard.id == secondCard.id;
+    let isMatch = true;
+    for (let i=0;i<(numberFlippedCard-1);i++){
+        if (arrayOfCards[i].id != arrayOfCards[i+1].id){
+            isMatch = false;
+        }
+    }
   
     if (isMatch) {
-      //numMatches++;
+      numMatches++;
       disableCards();
+
+      if (numMatches == 5){
+        numMatches = 0;
+        removeCards();
+        if (numberOfCards != 4){
+            createCards();
+        } else {
+            win();
+        }
+        
+     }
+
     } else {
       //numAttempts++;
       unflipCards();
     }
 }
 
+function removeCards(){
+    let inputLevel = document.getElementById("level-storage");
+    inputLevel.value = numberOfCards;
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(function(card){
+        while (card.hasChildNodes()){
+            card.removeChild(card.firstChild);
+        }
+        card.remove();
+    });
+}
+
 function unflipCards() {
     lockBoard = true;
     
+    console.log(arrayOfCards);
     setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
-      let firstImg = firstCard.querySelectorAll('.emoji-pairs-card');
-      firstImg.forEach(img => img.setAttribute("hidden", "hidden"));
-      let secondImg = secondCard.querySelectorAll('.emoji-pairs-card');
-      secondImg.forEach(img => img.setAttribute("hidden", "hidden"));
+        arrayOfCards.forEach(card => card.classList.remove('flip'));
+        arrayOfCards.forEach(card => card.querySelectorAll('.emoji-pairs-card').forEach(img => img.setAttribute("hidden", "hidden")));
   
       resetBoard();
     }, 1000);
 }
 
 function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-  
+    arrayOfCards.forEach(card => card.removeEventListener('click', flipCard));
+
     resetBoard();
 }
 
 function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+    [numberFlippedCard, lockBoard] = [0, false];
+    arrayOfCards = [];
+    arrayOfCards.push(0*(numberOfCards - 1));
 }
 
 // Create card divs and img elements (=> create cards on the board)
 function createCards(){
+    
+    let level = document.getElementById("level-storage").value;
 
     while (allPairs.length != 5){
         createCardCombination(skinImages, eyesImages, mouthImages);
     }
 
-    let cardFacesArray = [];
+    //console.log("Cards after createCardCombination in main function"+allPairs);
 
     // Assign how many cards a user should match (number of each card that should be generated)
     if (level == 1){
@@ -135,6 +168,7 @@ function createCards(){
         numberOfCards = 4;
         cardFacesArray = allPairs.concat(allPairs).concat(allPairs).concat(allPairs);
     } 
+    arrayOfCards.push(0*(numberOfCards - 1));
     cardsShuffle(cardFacesArray);
 
     const cardBoard = document.getElementById("cards-layout");
@@ -169,11 +203,68 @@ function createCards(){
         cardBoard.appendChild(card);
     }
 
-    
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => card.addEventListener('click', flipCard));
-    
 }
+
+function win(){
+        // Session is set
+        const cardBoard = document.getElementById("game-board");
+        const wrapper = document.createElement("div");
+        wrapper.setAttribute("id", "wrapper");
+        
+        const scoreMessage = document.createElement("h2");
+        scoreMessage.setAttribute("id", "h2-pairs");
+        const textNode = document.createTextNode("Your score: ");
+        scoreMessage.appendChild(textNode); 
+        cardBoard.appendChild(scoreMessage);
+
+        const submitScore = document.createElement("button");
+        submitScore.setAttribute("id", "button-on-pairs");
+        const textNode1 = document.createTextNode("Submit score");
+        submitScore.appendChild(textNode1);
+        wrapper.appendChild(submitScore);
+        cardBoard.appendChild(wrapper);
+
+        const playAgain = document.createElement("button"); 
+        playAgain.setAttribute("id", "button-on-pairs");
+        const textNode2 = document.createTextNode("Play again");
+        playAgain.appendChild(textNode2);
+        wrapper.appendChild(playAgain);
+        cardBoard.appendChild(wrapper);
+}
+
+/*function win(){
+    if (session_set) {
+        // Session is set
+        const cardBoard = document.getElementById("game-board");
+        const wrapper = document.createElement("div");
+        wrapper.setAttribute("id", "wrapper");
+        
+        const scoreMessage = document.createElement("h2");
+        scoreMessage.setAttribute("id", "h2-pairs");
+        const textNode = document.createTextNode("Your score: ");
+        scoreMessage.appendChild(textNode); 
+        cardBoard.appendChild(scoreMessage);
+
+        const submitScore = document.createElement("button");
+        submitScore.setAttribute("id", "button-on-pairs");
+        const textNode1 = document.createTextNode("Submit score");
+        submitScore.appendChild(textNode1);
+        wrapper.appendChild(submitScore);
+        cardBoard.appendChild(wrapper);
+
+        const playAgain = document.createElement("button"); 
+        playAgain.setAttribute("id", "button-on-pairs");
+        const textNode2 = document.createTextNode("Play again");
+        playAgain.appendChild(textNode2);
+        wrapper.appendChild(playAgain);
+        cardBoard.appendChild(wrapper);
+    } else {
+        // Session is not set
+        console.log('Session is not set');
+    }
+} */
 
 
 
